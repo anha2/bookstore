9c188a3a1f1345586898cfc83b5db0fb6ff89706
anha2 <anja.harju@outlook.com>
2024-02-04 11:23:09 +0200

    Täydennys

--- src/main/java/hh/sof3/bookstore/web/BookstoreController.java
+++ src/main/java/hh/sof3/bookstore/web/BookstoreController.java
@@ -1,15 +1,24 @@
 package hh.sof3.bookstore.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import hh.sof3.bookstore.domain.Book;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@ResponseBody
-
 public class BookstoreController {
-    @GetMapping("/index")
-    public String index() {
-        return "Hello";
+    @RequestMapping(value = { "/index", "*" }, method = RequestMethod.GET)
+    public String getBooks(Model model) {
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book("Teos", "Tekijä", 2024, "123", 50.0));
+        model.addAttribute("books", books);
+
+        return "index";
+
     }
+
 }
