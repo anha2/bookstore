1fafe1bcd5cf6b023e20aeeb1a28ea40825e4840
anha2 <anja.harju@outlook.com>
2024-02-11 18:20:58 +0200

    C3.4

--- src/main/java/hh/sof3/bookstore/domain/Book.java
+++ src/main/java/hh/sof3/bookstore/domain/Book.java
@@ -10,7 +10,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String title;
     private String author;
@@ -80,11 +80,11 @@ public class Book {
                 + ", price=" + price + "]";
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/hh/sof3/bookstore/web/BookController.java
+++ src/main/java/hh/sof3/bookstore/web/BookController.java
@@ -7,7 +7,9 @@ import org.springframework.ui.Model;
 import hh.sof3.bookstore.domain.Book;
 import hh.sof3.bookstore.domain.BookRepository;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -45,4 +47,29 @@ public class BookController {
         return "redirect:booklist";
     }
 
+    @SuppressWarnings("null")
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        Book book = bookRepository.findById(bookId).orElse(null);
+        model.addAttribute("book", book);
+        return "editbook";
+    }
+
+    @PostMapping("/update")
+    public String updateBook(@ModelAttribute("book") Book updatedBook) {
+        Book existingBook = bookRepository.findById(updatedBook.getId()).orElse(null);
+
+        if (existingBook != null) {
+            existingBook.setAuthor(updatedBook.getAuthor());
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setIsbn(updatedBook.getIsbn());
+            existingBook.setPublicationYear(updatedBook.getPublicationYear());
+            existingBook.setPrice(updatedBook.getPrice());
+
+            bookRepository.save(existingBook);
+        }
+
+        return "redirect:/booklist";
+
+    }
 }
