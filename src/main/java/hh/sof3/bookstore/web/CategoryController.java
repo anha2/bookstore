69821312dd1a18f1275485aa26fd1b3537910944
anha2 <anja.harju@outlook.com>
2024-02-23 12:11:52 +0200

    Tehtävä 4.2.

---
+++ src/main/java/hh/sof3/bookstore/web/CategoryController.java
@@ -0,0 +1,44 @@
+package hh.sof3.bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import hh.sof3.bookstore.domain.BookRepository;
+import hh.sof3.bookstore.domain.Category;
+import hh.sof3.bookstore.domain.CategoryRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CategoryController {
+    // @Autowired
+    // private BookRepository bookRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @RequestMapping(value = { "/categorylist", "*" }, method = RequestMethod.GET)
+    public String getCategories(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "categorylist"; // categorylist.html
+    }
+
+    @RequestMapping(value = "/addc")
+    public String addCategory(Model model) {
+        model.addAttribute("category", new Category());
+        return "addcategory"; // addcategory.html
+    }
+
+    @SuppressWarnings("null")
+
+    @PostMapping(value = "/savec")
+    public String save(Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categorylist";
+    }
+
+}
